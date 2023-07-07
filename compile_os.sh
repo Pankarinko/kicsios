bfff462890b7f0d19e4159df598addd169db9318
pankarinko <pankarinko@gmail.com>
2023-07-07 19:36:05 +0200

    Separate compilation steps

--- compile_os.sh
+++ compile_os.sh
@@ -1,3 +1,9 @@
 #! /usr/bin/bash
 
-riscv64-unknown-elf-gcc -O3 -nostdlib -nostartfiles -march=rv64g -mabi=lp64 -mcmodel=medany -fvisibility=hidden -T script.ld entry.S -o kernel
+mkdir -p build
+
+riscv64-unknown-elf-gcc -O3 -nostdlib -nostartfiles -march=rv64g -mabi=lp64 -mcmodel=medany -fvisibility=hidden -c entry.S -o build/asm_entry.o
+
+riscv64-unknown-elf-gcc -O3 -nostdlib -nostartfiles -march=rv64g -mabi=lp64 -mcmodel=medany -fvisibility=hidden -c entry.c -o build/c_entry.o
+
+riscv64-unknown-elf-gcc -O3 -nostdlib -nostartfiles -march=rv64g -mabi=lp64 -mcmodel=medany -fvisibility=hidden -T script.ld build/*.o -o kernel
--- entry.c
+++ entry.c
@@ -1,4 +1,4 @@
-void loop(void) {
+void c_entry(void) {
     volatile int *addr = (int*) 0x100000;
     *addr = 0x5555;
 }
