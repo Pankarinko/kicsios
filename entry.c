e93f77c3a8b7fb8f173f5a8b6e0ad76c5dcb5542
pankarinko <pankarinko@gmail.com>
2023-07-09 05:40:55 +0200

    Output one character:.

--- entry.c
+++ entry.c
@@ -1,4 +1,16 @@
+#include "src/kstd.h"
+
+void output_UART(void) {
+    volatile uint8 *base = (uint8*) 0x10000000;
+    uint8 init = 3;
+    *(base + 3) = init;
+    *base = 'a';
+}
+
+
 void c_entry(void) {
     volatile int *addr = (int*) 0x100000;
+    output_UART();
     *addr = 0x5555;
 }
+
