b8a722a9ca87055b8f68243fb34769b571da648b
Pankarinko <pankarinko@gmail.com>
2024-03-29 16:43:59 +0100

    Map all page tables in advance

--- src/vma.c
+++ src/vma.c
@@ -2,7 +2,12 @@
 #include "kstd.h"
 #include "alloc.h"
 
+// Address of the root page table
 usize *root_page_table;
+/* The frist free physical page
+Caution! It contains the PPN not the whole physical address (physical_address >> 12)
+*/
+usize *first_free_ppn;
 
 int initialize_vm() {
     set_root_page_table_bare();
@@ -14,11 +19,10 @@ int initialize_vm() {
 // Used for initializing the mapping
 void set_root_page_table_bare(void) {
     usize root_page_table = ROOT_PAGE_TABLE;
-    // TODO recursion
     usize set_satp = SET_ROOT_BARE;
     asm ("csrrw zero, satp, %0"
         :
-        : "r" (root_page_table));
+        : "r" (set_satp));
     zero_page(root_page_table);
 }
 
@@ -36,11 +40,49 @@ void zero_page(usize *page) {
     }
 }
 
-#define T 1
-#define P 0
+// Mapping the whole physical address space from 0x8000
+// This has to be done in BARE mode
+void map_tables() {
+    ptetype *pte = ROOT_PAGE_TABLE;
+    usize page_table_address = ROOT_PAGE_TABLE + PAGESIZE;
+    usize number_of_tables = (1 << VPNSIZE);
+    for (usize level = 0; level < LEVELS - 1; level++) {
+        for (usize i = 0; i < number_of_tables; i++) {
+            /* TODO Not really satisfied with this part, because 
+            it would be pretty useless to allocate the whole memory 
+            only for page tables
 
+            However, I will deal with this later. Now I will just assume that the RAM space is sufficient
+            */
+            if (page_table_address + PAGESIZE <= MEMORYSIZE) {
+            // TODO something on error
+            }
+            *pte = (ptetype) page_table_address | GLOBAL | VALID;
+            page_table_address += PAGESIZE;
+            pte += 1;
+        }
+        number_of_tables *= number_of_tables;
+    }
+    first_free_ppn = page_table_address >> 12;
+}
 
+// This function should only be called AFTER map_tables()
 void map_page(usize va) {
+    usize *address = ROOT_PAGE_TABLE;
+    usize pt_index = 0;
+    for (usize level = LEVELS; level > 1; level--) {
+        pt_index = (va >> (level)) & (MAXPTE << PTE_LOG); 
+        usize *address = *(usize*) (address + pt_index); //This still needs shifting somebits
+    }
+    *(ptetype*) address = va | READ | WRITE | EXECUTE | VALID | GLOBAL; 
+}
+
+// This is right now an identity function but who knows what happens later
+usize translate_address(usize va) {
+    return va;
+}
+
+/*void map_page(usize va) {
     ptetype page_addr = (ptetype) ROUNDDOWN_PAGE(va);
     ptetype current_page_table = root_page_table;
 
@@ -54,9 +96,7 @@ void map_page(usize va) {
         }
         current_page_table = pte ;
     }
-    /*TODO recursive page tables
-    */
-}
+}*/
 
 void map_kernel() {
     usize kernel_pages = ROUNDDOWN_PAGE(endkernel) - ROUNDDOWN_PAGE(startkernel);
